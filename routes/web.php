8377abea3e9f4595ce8294010d552fd326d379ac
KeithDev2020 <eire19@hotmail.com>
2021-06-02 16:58:02 +0100

    UI implemented, Login/Registration/Security implemented/ Database working and retrieving all items, not working=DB save and edit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\WeatherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/auth/login');
+});
+
+
+Route::post('/auth/save', [AuthController::class, 'save'])->name('auth.save');
+Route::post('/auth/check', [AuthController::class, 'check'])->name('auth.check');
+Route::get('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+Route::get('dashboard', function (){
+    return view('dashboard');
+});
+Route::get('dashboard', [WeatherController::class, 'show']);
+Route::get('delete/{forecast_id}', [WeatherController::class, 'delete']);
+Route::get('edit/{forecast_id}', [WeatherController::class, 'showData']);
+Route::post('add', [WeatherController::class, 'add'])->name('add');
+
+Route::group(['middleware'=>['AuthCheck']], function(){
+    Route::get('dashboard', [WeatherController::class, 'show']);
+    Route::get('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+    Route::get('/auth/register', [AuthController::class, 'register'])->name('auth.register');
 });
